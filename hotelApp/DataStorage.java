fb2c4bc440c5231056cb7cb8161382000c4e7be4
Nicholas Papano <nicholaspapano42@gmail.com>
2018-11-21 23:23:45 -0800

    minor comment adjustment, removed a TODO

--- hotelApp/DataStorage.java
+++ hotelApp/DataStorage.java
@@ -138,7 +138,6 @@ public class DataStorage
      */
     public boolean saveToFile()
     {
-        //TODO: Finish/test
         try
         {
             FileWriter accWriter = new FileWriter(accountFile);
